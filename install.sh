e4ed619f842f42e169b07dea57c5324ba45fabbe
Sondre Rudi <sonb@redpill-linpro.com>
2022-12-13 12:59:53 +0100

    adding stuff

---
+++ install.sh
@@ -0,0 +1,19 @@
+OSdetect='uname -a'
+if [[ $($OSdetect) == *"Darwin"* ]]; then 
+	echo "--- Installing for mac ---"
+	echo "--- Installing Brew ---"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo "--- Installing oh-my-zsh ---"
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	echo "--- Installing tmux-config ---"
+	./packages/tmux-config/install.sh
+	echo "--- Installing iTerm2 ---"
+	brew install iterm2
+else
+	echo "--- Installing for Linux ---"
+        echo "--- Installing oh-my-zsh ---"
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        echo "--- Installing tmux-config ---"
+        ./packages/tmux-config/install.sh
+
+fi
